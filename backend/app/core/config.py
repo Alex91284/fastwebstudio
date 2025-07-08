f77719b8e527d61265b76607a7a1f38f34ba9b93
Alex91284 <alexbolanosmunoz@gmail.com>
2025-07-08 17:55:09 -0500

    add .env.example file and update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./fastwebstudio.db"
+    DATABASE_URL: str 
     secret_key: str
     algorithm: str
     class Config:
