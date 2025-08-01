55fb549e2c5255d5ad4216ff236e59082d4131bc
Alex91284 <alexbolanosmunoz@gmail.com>
2025-08-01 11:13:28 -0500

    clean the project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,10 @@
-import { useEffect, useState } from "react"
 
 function App() {
-  const [msg, setMsg] = useState("Cargando...")
-
-  useEffect(() => {
-    fetch("http://localhost:8000/api/ping")
-      .then(res => res.json())
-      .then(data => setMsg(data.message))
-      .catch(() => setMsg("Error conectando con el backend"))
-  }, [])
-
   return (
-    <div className="p-8">
-      <div className="text-red-500 text-4xl font-bold">Hola Tailwind!</div>
-
-      <h1>FastWebStudio</h1>
-      <p>{msg}</p>
+    <div className="flex items-center justify-center min-h-screen text-gray-800 bg-gray-100">
+      <h1 className="text-3xl font-bold">FastWebStudio</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
