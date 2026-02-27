0a48189c6c92e387271e45faf9373478fcb4319a
Alex91284 <alexbolanosmunoz@gmail.com>
2026-02-27 11:25:29 -0500

    resolving problems with create projects

--- backend/app/models/page.py
+++ backend/app/models/page.py
@@ -1,13 +1,17 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime, func
 from sqlalchemy.orm import relationship
 from app.db.database import Base
+from sqlalchemy import UniqueConstraint
 
 class Page(Base):
     __tablename__ = "pages"
+    __table_args__ = (
+        UniqueConstraint("project_id", "slug", name="uix_slug_project"),
+    )
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String(120), nullable=False)
-    slug = Column(String(160), unique=True, nullable=False)
+    slug = Column(String(160),  nullable=False)
     content = Column(Text, nullable=True)
     order = Column(Integer, default=0)
     project_id = Column(Integer, ForeignKey("projects.id"), nullable=False)
